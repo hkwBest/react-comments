2cf19e841104dedb27deb562b96c22cbc9d47604
huangkewei <huangkewei@boco.com.cn>
2017-11-14 16:42:26 +0800

    webpack3

--- webpack.config.js
+++ webpack.config.js
@@ -15,30 +15,46 @@ module.exports = {
     },
 
     resolve:{
-        extensions:['', '.js','.jsx']
+        extensions:['.js','.jsx']
     },
 
     module: {
-        // preLoaders: [
-        //     // 报错 ？？？？？
-        //     {test: /\.(js|jsx)$/, loader: "eslint-loader", exclude: /node_modules/}
-        // ],
-        loaders: [
-            { test: /\.(js|jsx)$/, exclude: /node_modules/, loader: 'babel' },
-            { test: /\.less$/, exclude: /node_modules/, loader: 'style!css!postcss!less' },
-            { test: /\.css$/, exclude: /node_modules/, loader: 'style!css!postcss' },
-            { test:/\.(png|gif|jpg|jpeg|bmp)$/i, loader:'url-loader?limit=5000' },  // 限制大小5kb
-            { test:/\.(png|woff|woff2|svg|ttf|eot)($|\?)/i, loader:'url-loader?limit=5000'} // 限制大小小于5k
+        rules: [
+            {
+                test: /\.jsx?$/, // test 去判断是否为.js或.jsx,是的话就是进行es6和jsx的编译
+                exclude: /(node_modules|bower_components)/,
+                loader: 'babel-loader',
+                query: {
+                    presets: ['es2015', 'react']
+                }
+            },
+            {
+                test: /\.(less|css)?$/,
+                exclude: /(node_modules|bower_components)/,
+                use: [
+                    'style-loader',
+                    {
+                        loader: 'css-loader',
+                        options: {
+                            importLoaders: 1
+                        }
+                    },
+                    'postcss-loader',
+                    'less-loader'
+                ]
+            },
+            {
+                test: /\.(jpg|jpeg|gif|bmp|png|webp)?$/i,
+                exclude: /(node_modules|bower_components)/,
+                loader: 'file-loader'
+            },
+            {
+                test: /\.(woff|woff2|svg|ttf|eot)?$/i,
+                exclude: /(node_modules|bower_components)/,
+                loader: 'file-loader'
+            }
         ]
     },
-
-    eslint: {
-        configFile: '.eslintrc' // Rules for eslint
-    },
-
-    postcss: [
-        require('autoprefixer') //调用autoprefixer插件，例如 display: flex
-    ],
 
     plugins: [
         // html 模板插件
@@ -51,26 +67,25 @@ module.exports = {
 
         // 打开浏览器
         new OpenBrowserPlugin({
-          url: 'http://localhost:8080'
+            url: 'http://localhost:8080'
         }),
 
         // 可在业务 js 代码中使用 __DEV__ 判断是否是dev模式（dev模式下可以提示错误、测试报告等, production模式不提示）
         new webpack.DefinePlugin({
-          __DEV__: JSON.stringify(JSON.parse((process.env.NODE_ENV == 'dev') || 'false'))
+            __DEV__: JSON.stringify(JSON.parse((process.env.NODE_ENV == 'dev') || 'false'))
         })
     ],
 
     devServer: {
         proxy: {
-          // 凡是 `/api` 开头的 http 请求，都会被代理到 localhost:3000 上，由 koa 提供 mock 数据。
-          // koa 代码在 ./mock 目录中，启动命令为 npm run mock
-          '/api': {
-            target: 'http://localhost:3000',
-            secure: false
-          }
+            // 凡是 `/api` 开头的 http 请求，都会被代理到 localhost:3000 上，由 koa 提供 mock 数据。
+            // koa 代码在 ./mock 目录中，启动命令为 npm run mock
+            '/api': {
+                target: 'http://localhost:3000',
+                secure: false
+            }
         },
         contentBase: "./public", //本地服务器所加载的页面所在的目录
-        colors: true, //终端中输出结果为彩色
         historyApiFallback: true, //不跳转
         inline: true, //实时刷新
         hot: true  // 使用热加载插件 HotModuleReplacementPlugin
--- webpack.production.config.js
+++ webpack.production.config.js
@@ -1,84 +1,109 @@
 var path = require('path')
-var webpack = require('webpack');
+var webpack = require('webpack')
 var HtmlWebpackPlugin = require('html-webpack-plugin');
 var ExtractTextPlugin = require('extract-text-webpack-plugin');
+var OpenBrowserPlugin = require('open-browser-webpack-plugin');
+
+// var nodeModulesPath = path.resolve(__dirname, 'node_modules')
+// console.log(process.env.NODE_ENV)
 
 module.exports = {
-  entry: {
-    app: path.resolve(__dirname, 'app/index.jsx'),
-    // 将 第三方依赖 单独打包
-    vendor: [
-      'react', 
-      'react-dom', 
-      'react-redux', 
-      'react-router', 
-      'redux', 
-      'es6-promise', 
-      'whatwg-fetch', 
-      'immutable'
+    entry: {
+        app: path.resolve(__dirname, 'app/index.jsx'),
+        // 将 第三方依赖 单独打包
+        vendor: [
+            'react',
+            'react-dom',
+            'react-redux',
+            'react-router',
+            'react-router-dom',
+            'redux',
+            'es6-promise',
+            'whatwg-fetch'
+        ]
+    },
+    output: {
+        path: __dirname + "/build",
+        filename: "[name].[chunkhash:8].js",
+        publicPath: '/'
+    },
+
+    resolve:{
+        extensions:['.js','.jsx']
+    },
+
+    module: {
+        rules: [
+            {
+                test: /\.jsx?$/, // test 去判断是否为.js或.jsx,是的话就是进行es6和jsx的编译
+                exclude: /(node_modules|bower_components)/,
+                loader: 'babel-loader',
+                query: {
+                    presets: ['es2015', 'react']
+                }
+            },
+            {
+                test: /\.(less|css)?$/,
+                exclude: /(node_modules|bower_components)/,
+                use: ExtractTextPlugin.extract({
+                    fallback: "style-loader",
+                    use: "css-loader!less-loader"
+                })
+            },
+            {
+                test: /\.(jpg|jpeg|gif|bmp|png|webp)?$/i,
+                exclude: /(node_modules|bower_components)/,
+                loader: 'file-loader',
+                options: {
+                    name: 'img/[name].[ext]'
+                }
+            },
+            {
+                test: /\.(woff|woff2|svg|ttf|eot)?$/i,
+                exclude: /(node_modules|bower_components)/,
+                loader: 'file-loader',
+                options: {
+                    name: 'fonts/[name].[ext]'
+                }
+            }
+        ]
+    },
+
+    plugins: [
+        // webpack 内置的 banner-plugin
+        new webpack.BannerPlugin("Copyright by wangfupeng1988@github.com."),
+
+        // html 模板插件
+        new HtmlWebpackPlugin({
+            template: __dirname + '/app/index.tmpl.html'
+        }),
+
+        // 定义为生产环境，编译 React 时压缩到最小
+        new webpack.DefinePlugin({
+            'process.env':{
+                'NODE_ENV': JSON.stringify(process.env.NODE_ENV)
+            }
+        }),
+
+        new webpack.optimize.UglifyJsPlugin({
+            compress: {
+                //supresses warnings, usually from module minification
+                warnings: false
+            }
+        }),
+
+        // 分离CSS和JS文件
+        new ExtractTextPlugin('[name].[chunkhash:8].css'),
+
+        // 提供公共代码
+        new webpack.optimize.CommonsChunkPlugin({
+            name: 'vendor',
+            filename: '[name].[chunkhash:8].js'
+        }),
+
+        // 可在业务 js 代码中使用 __DEV__ 判断是否是dev模式（dev模式下可以提示错误、测试报告等, production模式不提示）
+        new webpack.DefinePlugin({
+            __DEV__: JSON.stringify(JSON.parse((process.env.NODE_ENV == 'dev') || 'false'))
+        })
     ]
-  },
-  output: {
-    path: __dirname + "/build",
-    filename: "[name].[chunkhash:8].js",
-    publicPath: '/'
-  },
-
-  resolve:{
-      extensions:['', '.js','.jsx']
-  },
-
-  module: {
-    loaders: [
-        { test: /\.(js|jsx)$/, exclude: /node_modules/, loader: 'babel' },
-        { test: /\.less$/, exclude: /node_modules/, loader: ExtractTextPlugin.extract('style', 'css!postcss!less') },
-        { test: /\.css$/, exclude: /node_modules/, loader: ExtractTextPlugin.extract('style', 'css!postcss') },
-        { test:/\.(png|gif|jpg|jpeg|bmp)$/i, loader:'url-loader?limit=5000&name=img/[name].[chunkhash:8].[ext]' },
-        { test:/\.(png|woff|woff2|svg|ttf|eot)($|\?)/i, loader:'url-loader?limit=5000&name=fonts/[name].[chunkhash:8].[ext]'}
-    ]
-  },
-  postcss: [
-    require('autoprefixer')
-  ],
-
-  plugins: [
-    // webpack 内置的 banner-plugin
-    new webpack.BannerPlugin("Copyright by wangfupeng1988@github.com."),
-
-    // html 模板插件
-    new HtmlWebpackPlugin({
-        template: __dirname + '/app/index.tmpl.html'
-    }),
-
-    // 定义为生产环境，编译 React 时压缩到最小
-    new webpack.DefinePlugin({
-      'process.env':{
-        'NODE_ENV': JSON.stringify(process.env.NODE_ENV)
-      }
-    }),
-
-    // 为组件分配ID，通过这个插件webpack可以分析和优先考虑使用最多的模块，并为它们分配最小的ID
-    new webpack.optimize.OccurenceOrderPlugin(),
-    
-    new webpack.optimize.UglifyJsPlugin({
-        compress: {
-          //supresses warnings, usually from module minification
-          warnings: false
-        }
-    }),
-    
-    // 分离CSS和JS文件
-    new ExtractTextPlugin('[name].[chunkhash:8].css'), 
-    
-    // 提供公共代码
-    new webpack.optimize.CommonsChunkPlugin({
-      name: 'vendor',
-      filename: '[name].[chunkhash:8].js'
-    }),
-
-    // 可在业务 js 代码中使用 __DEV__ 判断是否是dev模式（dev模式下可以提示错误、测试报告等, production模式不提示）
-    new webpack.DefinePlugin({
-      __DEV__: JSON.stringify(JSON.parse((process.env.NODE_ENV == 'dev') || 'false'))
-    })
-  ]
 }
